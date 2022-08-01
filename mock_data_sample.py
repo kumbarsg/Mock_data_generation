8d25d19ee427fe11b88f41a727163639ea3b7523
kumbarsg <saiganesh.kumbar@clairvoyantsoft.com>
2022-08-01 08:00:43 -0500

    Reformatted the file using black

--- mock_data_sample.py
+++ mock_data_sample.py
@@ -8,75 +8,114 @@ from random import *
 
 fake = Faker()
 
+
 class MyProvider(BaseProvider):
     __provider__ = "types"
     __provider__ = "description"
     __provider__ = "fruit"
 
     def types(self):
-        types = ['ASSAULT', 'BATTERY', 'BURGLARY', 'CRIMINAL DAMAGE', 
-        'MOTOR VEHICLE THEFT', 'NARCOTICS', 'OFFENSE INVOLVING CHILDREN', 'OTHER OFFENSE']
+        types = [
+            "ASSAULT",
+            "BATTERY",
+            "BURGLARY",
+            "CRIMINAL DAMAGE",
+            "MOTOR VEHICLE THEFT",
+            "NARCOTICS",
+            "OFFENSE INVOLVING CHILDREN",
+            "OTHER OFFENSE",
+        ]
         return choice(types)
 
     def description(self):
-        description = ['SIMPLE', 'AGGREVATED', 'RECKLESS CONDUCT', 'UNLAWFUL']
+        description = ["SIMPLE", "AGGREVATED", "RECKLESS CONDUCT", "UNLAWFUL"]
         return choice(description)
 
     def location_description(self):
         location_description = [
-        "RESIDENCE", "OFFICE","STREET", "PARKING LOT", "STORE", "PARKING"
-    ]
+            "RESIDENCE",
+            "OFFICE",
+            "STREET",
+            "PARKING LOT",
+            "STORE",
+            "PARKING",
+        ]
         return choice(location_description)
 
 
 fake.add_provider(MyProvider)
 
 now = datetime.datetime.now()
-now_date_time = now.strftime( "%d/%m/%Y %H:%M:%S %p")
+now_date_time = now.strftime("%d/%m/%Y %H:%M:%S %p")
+
 
 def datagenerate(records, headers):
-    
-    with open("Crimes_2001_to_Present.csv", 'a') as csvFile:
+
+    with open("Crimes_2001_to_Present.csv", "wt") as csvFile:
         writer = csv.DictWriter(csvFile, fieldnames=headers)
-        #writer.writeheader() #Only while creating a new file
+        # Only while creating a new file, we open in 'wt' mode and write header
+        writer.writeheader()
         for i in range(records):
             location_lat = str(fake.latitude())
             location_lon = str(fake.longitude())
-            location_co = '(' + location_lat + ', ' + location_lon + ')'
-            
-            writer.writerow({
+            location_co = "(" + location_lat + ", " + location_lon + ")"
 
-                    
-                    "ID" : 1000801 + i,
-                    "Case_Number": fake.bothify(text= 'H?######', letters = 'PQRST'), 
-                    "Date": fake.date_time_this_year().strftime('%x %X') + ' ' + fake.am_pm(),
+            writer.writerow(
+                {
+                    "ID": i,
+                    "Case_Number": fake.bothify(text="H?######", letters="PQRST"),
+                    "Date": fake.date_time_this_year().strftime("%x %X")
+                    + " "
+                    + fake.am_pm(),
                     "Block": fake.street_address(),
                     "IUCR": fake.pyint(),
                     "Primary_Type": fake.types(),
                     "Description": fake.description(),
                     "Location_Description": fake.location_description(),
                     "Arrest": fake.boolean(chance_of_getting_true=25),
-                    "Domestic":fake.boolean(chance_of_getting_true=25),
+                    "Domestic": fake.boolean(chance_of_getting_true=25),
                     "Beat": fake.pyint(),
-                    "District": fake.pyint(1,25),
-                    "Ward":fake.pyint(1,50),
-                    "Community_Area":fake.pyint(1,75),
-                    "FBI_Code": fake.pyint(1,30),
+                    "District": fake.pyint(1, 25),
+                    "Ward": fake.pyint(1, 50),
+                    "Community_Area": fake.pyint(1, 75),
+                    "FBI_Code": fake.pyint(1, 30),
                     "XCoordinate": fake.pyint(1000001, 9999999),
                     "YCoordinate": fake.pyint(1000001, 9999999),
-                    "Year": fake.date_between_dates(datetime.date(2000,1,1), datetime.date(2022,1,1)).year,
+                    "Year": fake.date_between_dates(
+                        datetime.date(2000, 1, 1), datetime.date(2022, 1, 1)
+                    ).year,
                     "Updated_On": now_date_time,
                     "Latitude": location_lat,
-                    "Longitude":location_lat,
-                    "Location": location_co
+                    "Longitude": location_lat,
+                    "Location": location_co,
+                }
+            )
 
-                    })
 
-                   
-    
-
-records = 100000000
-headers = [ "ID","Case_Number","Date","Block","IUCR","Primary_Type","Description","Location_Description","Arrest","Domestic",
-        "Beat","District","Ward","Community_Area","FBI_Code","XCoordinate","YCoordinate","Year","Updated_On","Latitude","Longitude","Location" ]
+records = 100000
+headers = [
+    "ID",
+    "Case_Number",
+    "Date",
+    "Block",
+    "IUCR",
+    "Primary_Type",
+    "Description",
+    "Location_Description",
+    "Arrest",
+    "Domestic",
+    "Beat",
+    "District",
+    "Ward",
+    "Community_Area",
+    "FBI_Code",
+    "XCoordinate",
+    "YCoordinate",
+    "Year",
+    "Updated_On",
+    "Latitude",
+    "Longitude",
+    "Location",
+]
 datagenerate(records, headers)
 print("CSV generation complete!")
